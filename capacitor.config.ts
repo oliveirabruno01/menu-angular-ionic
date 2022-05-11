3c4ed9cc263402af15e58a32a2312ecc98fcd77b
oliveirabruno01 <brunocabeludo321@gmail.com>
2022-05-11 18:08:26 -0300

    Ionic start app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'menu-angular-ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
